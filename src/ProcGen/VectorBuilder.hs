9b54b300f539c18de28213f1fd764466a12b458f
Ramin Honary <ramin.honary@gmail.com>
2018-07-27 19:22:35 +0900

    Renamed 'BuildContiguous' to 'VectorBuilder', reflecting how the module's name changed since I first thought of it.

--- src/ProcGen/VectorBuilder.hs
+++ src/ProcGen/VectorBuilder.hs
@@ -17,34 +17,34 @@ import qualified Data.Vector.Unboxed               as UVec
 
 -- | A function type for buliding mutable 'UVec.Vector's with a function that uses a cursor to move
 -- around withtin a mutable 'Vec.Vector'.
-newtype BuildContiguous vec elem m a
-  = BuildContiguous
-    { unwrapBuildContiguous :: StateT (BuildContiguousState vec elem m) m a
+newtype VectorBuilder vec elem m a
+  = VectorBuilder
+    { unwrapVectorBuilder :: StateT (VectorBuilderState vec elem m) m a
     }
   deriving (Functor, Applicative, Monad)
 
-instance MonadTrans (BuildContiguous vec elem) where
-  lift = BuildContiguous . lift
+instance MonadTrans (VectorBuilder vec elem) where
+  lift = VectorBuilder . lift
 
-instance Monad m => MonadState (BuildContiguousState vec elem m) (BuildContiguous vec elem m) where
-  state = BuildContiguous . state
+instance Monad m => MonadState (VectorBuilderState vec elem m) (VectorBuilder vec elem m) where
+  state = VectorBuilder . state
 
-data BuildContiguousState vec elem m
-  = BuildContiguousState
+data VectorBuilderState vec elem m
+  = VectorBuilderState
     { theBuilderCursor :: !Int
     , theBuilderVector :: !(vec (PrimState m) elem)
     }
 
 -- | Given an initial vector size, allocate an 'MVec.STVector' of that size and then evaluate a
--- 'BuildContiguous' function to populate the 'MVec.STVector'. When 'BuildContiguous' function
+-- 'VectorBuilder' function to populate the 'MVec.STVector'. When 'VectorBuilder' function
 -- evaluation completes, freeze the 'MVec.STVector' to a 'UVec.Vector' and eliminate the monad
 -- entirely, becoming a pure function.
-runBuildContiguousST
+runVectorBuilderST
   :: (UVec.Unbox elem)
-  => Int -> (forall s . BuildContiguous UVec.MVector elem (ST s) void) -> UVec.Vector elem
-runBuildContiguousST size build = UVec.create $ do
+  => Int -> (forall s . VectorBuilder UVec.MVector elem (ST s) void) -> UVec.Vector elem
+runVectorBuilderST size build = UVec.create $ do
   vec <- MVec.new size
-  evalStateT (unwrapBuildContiguous $ build >> use builderVector) BuildContiguousState
+  evalStateT (unwrapVectorBuilder $ build >> use builderVector) VectorBuilderState
     { theBuilderCursor = 0
     , theBuilderVector = vec
     }
@@ -53,26 +53,26 @@ runBuildContiguousST size build = UVec.create $ do
 -- element value.
 builderCursor
   :: (PrimMonad m, MVec.MVector vec elem)
-  => Lens' (BuildContiguousState vec elem m) Int
+  => Lens' (VectorBuilderState vec elem m) Int
 builderCursor = lens theBuilderCursor $ \ a b -> a{ theBuilderCursor = b }
 
 -- | A lens to access the vector that is currently being filled with @elem@ values.
 builderVector
   :: (PrimMonad m, MVec.MVector vec elem)
-  => Lens' (BuildContiguousState vec elem m) (vec (PrimState m) elem)
+  => Lens' (VectorBuilderState vec elem m) (vec (PrimState m) elem)
 builderVector = lens theBuilderVector $ \ a b -> a{ theBuilderVector = b }
 
 -- | Take the value of an @elem@ at the current 'builderCursor' in the 'builderVector'.
-builderGetElem :: (PrimMonad m, MVec.MVector vec elem) => BuildContiguous vec elem m elem
+builderGetElem :: (PrimMonad m, MVec.MVector vec elem) => VectorBuilder vec elem m elem
 builderGetElem = MVec.read <$> use builderVector <*> use builderCursor >>= lift
 
 -- | Reposition the 'builderCursor'. Returns the value of the 'builderCursor' as it was set before the
 -- @('Int' -> 'Int')@ function was applied.
-modifyCursor :: (PrimMonad m, MVec.MVector vec elem) => (Int -> Int) -> BuildContiguous vec elem m Int
+modifyCursor :: (PrimMonad m, MVec.MVector vec elem) => (Int -> Int) -> VectorBuilder vec elem m Int
 modifyCursor = (<*) (use builderCursor) . (builderCursor %=)
 
 -- | Get the current length of the vector allocation size being built.
-maxBuildLength :: (PrimMonad m, MVec.MVector vec elem) => BuildContiguous vec elem m Int
+maxBuildLength :: (PrimMonad m, MVec.MVector vec elem) => VectorBuilder vec elem m Int
 maxBuildLength = MVec.length <$> use builderVector
 
 -- | Force re-allocation of the vector using 'MVec.grow' or 'MVec.take', depending on whether the
@@ -81,7 +81,7 @@ maxBuildLength = MVec.length <$> use builderVector
 resizeVector
   :: (PrimMonad m, MVec.MVector vec elem)
   => (Int -> Int)
-  -> BuildContiguous vec elem m ()
+  -> VectorBuilder vec elem m ()
 resizeVector f = do
   oldsize <- maxBuildLength
   let newsize = f oldsize
@@ -91,46 +91,46 @@ resizeVector f = do
      else MVec.take newsize <$> use builderVector >>= (builderVector .=)
 
 -- | Place an @elem@ value at the current 'builderCursor', and then increment the 'builderCursor'.
-buildStep :: (PrimMonad m, MVec.MVector vec elem) => elem -> BuildContiguous vec elem m ()
+buildStep :: (PrimMonad m, MVec.MVector vec elem) => elem -> VectorBuilder vec elem m ()
 buildStep e = do
   MVec.write <$> use builderVector <*> use builderCursor <*> pure e >>= lift
   builderCursor += 1
 
 -- | Get the @elem@ value at the current 'builderCursor', and then increment the 'builderCursor'.
-buildTake1 :: (PrimMonad m, MVec.MVector vec elem) => BuildContiguous vec elem m elem
+buildTake1 :: (PrimMonad m, MVec.MVector vec elem) => VectorBuilder vec elem m elem
 buildTake1 =
   (MVec.read <$> use builderVector <*> use builderCursor >>= lift) <*
   (builderCursor += 1)
 
 -- | The 'buildTake1' value applied 2 times to return a double, and thus advancing the
 -- 'builderCursor' by 2 indicies as well.
-buildTake2 :: (PrimMonad m, MVec.MVector vec elem) => BuildContiguous vec elem m (elem, elem)
+buildTake2 :: (PrimMonad m, MVec.MVector vec elem) => VectorBuilder vec elem m (elem, elem)
 buildTake2 = (,) <$> buildTake1 <*> buildTake1
 
 -- | The 'buildTake1' value applied 3 times to return a tripple, and thus advancing the
 -- 'builderCursor' by 3 indicies as well.
-buildTake3 :: (PrimMonad m, MVec.MVector vec elem) => BuildContiguous vec elem m (elem, elem, elem)
+buildTake3 :: (PrimMonad m, MVec.MVector vec elem) => VectorBuilder vec elem m (elem, elem, elem)
 buildTake3 = (,,) <$> buildTake1 <*> buildTake1 <*> buildTake1
 
 -- | The 'buildTake1' value applied 4 times to return a tripple, and thus advancing the
 -- 'builderCursor' by 4 indicies as well.
 buildTake4
   :: (PrimMonad m, MVec.MVector vec elem)
-  => BuildContiguous vec elem m (elem, elem, elem, elem)
+  => VectorBuilder vec elem m (elem, elem, elem, elem)
 buildTake4 = (,,,) <$> buildTake1 <*> buildTake1 <*> buildTake1 <*> buildTake1
 
 -- | The 'buildTake1' value applied 5 times to return a tripple, and thus advancing the
 -- 'builderCursor' by 5 indicies as well.
 buildTake5
   :: (PrimMonad m, MVec.MVector vec elem)
-  => BuildContiguous vec elem m (elem, elem, elem, elem, elem)
+  => VectorBuilder vec elem m (elem, elem, elem, elem, elem)
 buildTake5 = (,,,,) <$> buildTake1 <*> buildTake1 <*> buildTake1 <*> buildTake1 <*> buildTake1
 
 -- | The 'buildTake1' value applied 5 times to return a tripple, and thus advancing the
 -- 'builderCursor' by 5 indicies as well.
 buildTake6
   :: (PrimMonad m, MVec.MVector vec elem)
-  => BuildContiguous vec elem m (elem, elem, elem, elem, elem, elem)
+  => VectorBuilder vec elem m (elem, elem, elem, elem, elem, elem)
 buildTake6 = (,,,,,) <$> buildTake1 <*> buildTake1 <*>
   buildTake1 <*> buildTake1 <*> buildTake1 <*> buildTake1
 
@@ -167,15 +167,15 @@ buildMapRange
   :: (PrimMonad m, MVec.MVector vec elem)
   => MakeRange
   -> (Int -> elem -> elem)
-  -> BuildContiguous vec elem m ()
+  -> VectorBuilder vec elem m ()
 buildMapRange makeRange f = buildMapRangeM makeRange (\ i -> pure . f i)
 
 -- | A version of 'buildMapRange' that takes a monadic mapping function.
 buildMapRangeM
   :: (PrimMonad m, MVec.MVector vec elem)
   => MakeRange
-  -> (Int -> elem -> BuildContiguous vec elem m elem)
-  -> BuildContiguous vec elem m ()
+  -> (Int -> elem -> VectorBuilder vec elem m elem)
+  -> VectorBuilder vec elem m ()
 buildMapRangeM makeRange f = do
   vec   <- use builderVector
   range <- makeRange <$> use builderCursor <*> maxBuildLength
@@ -187,15 +187,15 @@ buildScanRange
   :: (PrimMonad m, MVec.MVector vec elem)
   => MakeRange -> st
   -> (Int -> elem -> st -> (Maybe elem, st))
-  -> BuildContiguous vec elem m st
+  -> VectorBuilder vec elem m st
 buildScanRange makeRange st f = buildScanRangeM makeRange st (\ i e -> pure . f i e)
 
 -- | A version of 'buildScanRange' that takes a monadic scanning function.
 buildScanRangeM
   :: (PrimMonad m, MVec.MVector vec elem)
   => MakeRange -> st
-  -> (Int -> elem -> st -> BuildContiguous vec elem m (Maybe elem, st))
-  -> BuildContiguous vec elem m st
+  -> (Int -> elem -> st -> VectorBuilder vec elem m (Maybe elem, st))
+  -> VectorBuilder vec elem m st
 buildScanRangeM makeRange st f = do
   vec <- use builderVector
   makeRange <$> use builderCursor <*> maxBuildLength >>=
@@ -206,8 +206,8 @@ buildScanRangeM makeRange st f = do
 buildFoldRangeM
   :: (PrimMonad m, MVec.MVector vec elem)
   => MakeRange -> st
-  -> (Int -> elem -> st -> BuildContiguous vec elem m st)
-  -> BuildContiguous vec elem m st
+  -> (Int -> elem -> st -> VectorBuilder vec elem m st)
+  -> VectorBuilder vec elem m st
 buildFoldRangeM makeRange st f = do
   vec <- use builderVector
   makeRange <$> use builderCursor <*> maxBuildLength >>=
