181f852472472babad16a1e48194e5876afb3ba8
Ramin Honary <ramin.honary@gmail.com>
2018-09-02 00:46:50 +0900

    Completed defining the fundamental score-writing functions. 'PlayedNote' no longer contain it's own duration value, durations shall from now be decided entirely by the context of the enclosing 'Measure' data structure.

--- src/ProcGen/Music/Composition.hs
+++ src/ProcGen/Music/Composition.hs
@@ -36,12 +36,13 @@
 module ProcGen.Music.Composition
   ( CommonChordProg(..),
     -- * Individual Notes
-    PlayedNote(..), NoteReference, untied, NoteValue(..), Strength(..),
+    Note(..), makeNote, NoteReference, untied, NoteValue(..), noteValue, Strength(..),
     -- * Arranging Notes
     Measure(..), makeMeasure,
-    SubDiv(..), RoleNotes(..), roleNote, sequenceMeasure,
+    SubDiv(..), RoleNotes(..), roleNote, sequenceMeasure, setNoteDurations,
     -- * Composing Music
-    Composition, newComposition, measure, notes,
+    Composition, newComposition, measure,
+    notes, rest, tie, addNote, play, playNotes, playTie, score,
     module ProcGen.Arbitrary,
     module Control.Monad.State.Class,
   ) where
@@ -84,30 +85,36 @@ data CommonChordProg
 
 ----------------------------------------------------------------------------------------------------
 
--- | A 'Composition' is constructed of a list of 'PlayedNote's each played at a point in time.
-data PlayedNote
-  = RestNote
-    { playedDuration  :: !Duration
-    }
-    -- ^ indicates that nothing is played for the duration of the 'SubDiv'.
-  | TiedNote
-    { playedNoteID    :: !NoteReference
-      -- ^ This reference refers to the unique ID of this played note, not the note to which it is
-      -- tied. The 'PlayedNote' refers to this note's 'playedNoteID' in the 'playedTied' field if it
-      -- is tied.
-    , playedDuration  :: !Duration
-    }
+-- | A 'Note' with a 'ProcGen.Types.Duration'. Notes are associated with 'ProcGen.Types.Moment's
+-- only in 'Map.Map' data structures within the 'Measure' data type.
+data Interval note = Interval !Duration !note
+  deriving (Eq, Ord, Show)
+
+-- | A 'Composition' is constructed of a list of 'Note's each played at a point in time.
+data Note
+  = RestNote -- ^ indicates that nothing is played for the duration of the 'SubDiv'.
     -- ^ This must refer to a note that was played at an earlier point in the composition.
   | PlayedNote
     { playedNoteID    :: !NoteReference
-    , playedDuration  :: !Duration
+    , playedTiedID    :: !NoteReference
+      -- ^ Set to 'untied' if this note is not tied. If not 'untied' (if it is tied) it must refer
+      -- to the 'playedNotID' of 'Note' defined with the 'TiedNote' constructor.
     , playedStrength  :: !Strength
     , playedNoteValue :: !NoteValue
-    , playedTied      :: !NoteReference
-      -- ^ Set to 'untied' if this note is not tied. If not 'untied' (if it is tied) it must refer
-      -- to the 'playedNotID' of 'PlayedNote' defined with the 'TiedNote' constructor.
     }
   deriving (Eq, Ord, Show)
+
+-- | Construct a note from a 'Strength' and zero or more 'ProcGen.Music.KeyFreq88.KeyIndex' values
+-- which refer to notes on an 88-key piano keyboard.
+makeNote :: NoteReference -> Strength -> [KeyIndex] -> Note
+makeNote tied strength = \ case
+  []   -> RestNote
+  a:ax -> PlayedNote
+    { playedNoteID    = untied -- ^ allow 'addNote' to initialize this to the correct value
+    , playedTiedID    = tied
+    , playedStrength  = strength
+    , playedNoteValue = noteValue a ax
+    }
 
 -- | Every note in a 'Composition' has a unique index value or "reference". The 'Composition' keeps
 -- track of each unique note played in order to evaluate "tied notes" which are notes are held for
@@ -115,11 +122,11 @@ data PlayedNote
 -- note. 
 newtype NoteReference = NoteReference Int deriving (Eq, Ord, Show)
 
--- | A 'PlayedNote' that is not tied.
+-- | A 'Note' that is not tied.
 untied :: NoteReference
 untied = NoteReference minBound
 
--- | The value of the note played in a 'PlayedNote'.
+-- | The value of the note played in a 'Note'.
 data NoteValue
   = Single !Word8
     -- ^ Play a single note, the 'Data.Wordl.Word8' value will be used as an index to a table
@@ -128,8 +135,15 @@ data NoteValue
   deriving (Eq, Ord, Show, Read)
 
 -- | How hard the note is played, it is a fuzzy value that maps to 'ProcGen.Types.Amplitude'.
-data Strength = Fortisimo | Forte | MezzoForte | MezzoMezzo | MezzoPiano | Piano | Pianismo
+data Strength = Pianismo | Piano | MezzoPiano | MezzoMezzo | MezzoForte | Forte | Fortisimo
   deriving (Eq, Ord, Show, Read, Enum)
+
+-- | Construct a 'NoteValue'. It is better to just use 'makeNote' rather than ever call this
+-- function directly.
+noteValue :: KeyIndex -> [KeyIndex] -> NoteValue
+noteValue a = \ case
+  [] -> Single $ keyIndexToWord8 a
+  ax -> Chord $ Unboxed.fromList $ keyIndexToWord8 <$> a:ax
 
 ----------------------------------------------------------------------------------------------------
 
@@ -182,7 +196,8 @@ roleNote t dt = RoleNotes . Map.singleton t . (,) dt
 
 -- | A 'Measure' sub-divides the given initial 'ProcGen.Types.Duration' into several sub-intervals
 -- associated with the leaf elements. This function converts a 'Measure' into a mapping from the
--- start time to the @('ProcGen.Types.Duration', leaf)@ pair.
+-- start time to the @('ProcGen.Types.Duration', leaf)@ pair. When the @leaf@ type is unified with
+-- 'Note', it is helpful to evaluate the resulting 'RoleNotes' with 'setNoteDurations'.
 sequenceMeasure :: Moment -> Duration -> Measure leaf -> RoleNotes (Duration, leaf)
 sequenceMeasure t0 dt0 msur = loop dt0 mempty (t0 + playMetaOffset msur, playNoteTree msur) where
   loop dt0 map (t0, subdiv) = if t0 >= playMetaCutoff msur then map else case subdiv of
@@ -190,6 +205,13 @@ sequenceMeasure t0 dt0 msur = loop dt0 mempty (t0 + playMetaOffset msur, playNot
     SubDivBranch vec -> if Boxed.null vec then mempty else
       let dt = dt0 / realToFrac (Boxed.length vec) in
       foldl (loop dt) map $ zip (iterate (+ dt) t0) (Boxed.toList vec)
+
+-- | For a 'RoleNotes' containing a tuple @('Duration', 'Note')@ pair as what is constructed
+-- by the 'sequenceMeasure' function, this sets the 'ProcGen.Types.Duration' value in the
+-- 'Prelude.fst' of the tuple as the 'playedDuration' of each 'Note' in the 'Prelude.snd' of
+-- the tuple.
+setNoteDurations :: RoleNotes (Duration, Note) -> RoleNotes (Interval Note)
+setNoteDurations = fmap $ uncurry Interval
 
 ----------------------------------------------------------------------------------------------------
 
@@ -201,15 +223,19 @@ newtype ComposeT m a = ComposeT (StateT (Composition m) m a)
 -- 'Control.Monad.ST.ST' monad, or in the 'IO' monad.
 data Composition m
   = Composition
-    { theCompositionNoteCount   :: !Int
-    , theCompositionRandGen     :: !TFGen
-    , theCompositionNotes       :: !(Mutable.MVector (PrimState m) PlayedNote)
-    , theCompositionMeasures    :: !(Mutable.MVector (PrimState m) (Measure NoteReference))
-    , theCompositionDivSize     :: !Int
-    , theCompositionCurrentDiv  :: [NoteReference]
+    { theCompositionNoteCount    :: !Int
+    , theCompositionNotes        :: !(Mutable.MVector (PrimState m) Note)
+    , theCompositionRandGen      :: !TFGen
+    , theCompositionMeasureTime  :: !Duration
+    , theCompositionMeasureCount :: !Int
+    , theCompositionMeasures     :: !(Mutable.MVector (PrimState m) (Measure NoteReference))
+    , theCompositionDivSize      :: !Int
+    , theCompositionCurrentDiv   :: [NoteReference]
     }
 
 instance Monad m => MonadState (Composition m) (ComposeT m) where { state = ComposeT . state; }
+
+instance MonadTrans ComposeT where { lift = ComposeT . lift; }
 
 instance (Semigroup a, Monad m) => Semigroup (ComposeT m a) where { (<>) a b = (<>) <$> a <*> b; }
 
@@ -224,12 +250,14 @@ newComposition gen = do
   notes    <- Mutable.new 256
   measures <- Mutable.new 64
   return Composition
-    { theCompositionNoteCount   = 0
-    , theCompositionRandGen     = gen
-    , theCompositionMeasures    = measures
-    , theCompositionNotes       = notes
-    , theCompositionDivSize     = 0
-    , theCompositionCurrentDiv  = []
+    { theCompositionNoteCount    = 0
+    , theCompositionNotes        = notes
+    , theCompositionRandGen      = gen
+    , theCompositionMeasureTime  = 4.0
+    , theCompositionMeasureCount = 0
+    , theCompositionMeasures     = measures
+    , theCompositionDivSize      = 0
+    , theCompositionCurrentDiv   = []
     }
 
 -- TODO: newCompositionIO, newCompositionPure, runComposeIO, runComposePure
@@ -239,12 +267,20 @@ compositionNoteCount :: Lens' (Composition m) Int
 compositionNoteCount = lens theCompositionNoteCount $ \ a b -> a{ theCompositionNoteCount = b }
 
 -- not for export
+compositionNotes :: Lens' (Composition m) (Mutable.MVector (PrimState m) Note)
+compositionNotes = lens theCompositionNotes $ \ a b -> a{ theCompositionNotes = b }
+
+-- not for export
 compositionRandGen :: Lens' (Composition m) TFGen
 compositionRandGen = lens theCompositionRandGen $ \ a b -> a{ theCompositionRandGen = b }
 
 -- not for export
-compositionNotes :: Lens' (Composition m) (Mutable.MVector (PrimState m) PlayedNote)
-compositionNotes = lens theCompositionNotes $ \ a b -> a{ theCompositionNotes = b }
+compositionMeasureTime :: Lens' (Composition m) Duration
+compositionMeasureTime = lens theCompositionMeasureTime $ \ a b -> a{ theCompositionMeasureTime = b }
+
+-- not for export
+compositionMeasureCount :: Lens' (Composition m) Int
+compositionMeasureCount = lens theCompositionMeasureCount $ \ a b -> a{ theCompositionMeasureCount = b }
 
 -- not for export
 compositionMeasures :: Lens' (Composition m) (Mutable.MVector (PrimState m) (Measure NoteReference))
@@ -273,13 +309,16 @@ measure :: Monad m => ComposeT m () -> ComposeT m (Measure NoteReference)
 measure compose = do
   oldlist <- use compositionCurrentDiv
   oldsize <- use compositionDivSize
-  compositionCurrentDiv .= []
-  compositionDivSize    .= 0
+  oldtime <- use compositionMeasureTime
+  compositionCurrentDiv  .= []
+  compositionDivSize     .= 0
+  compositionMeasureTime %= (/ 2.0)
   compose
   newlist <- fmap SubDivLeaf <$> use compositionCurrentDiv
   newsize <- use compositionDivSize
-  compositionCurrentDiv .= oldlist
-  compositionDivSize    .= oldsize
+  compositionCurrentDiv  .= oldlist
+  compositionDivSize     .= oldsize
+  compositionMeasureTime .= oldtime
   return makeMeasure
     { playNoteTree = SubDivBranch $ Boxed.create $ do
         vec <- Mutable.new newsize
@@ -287,11 +326,83 @@ measure compose = do
         return vec
     }
 
+-- TODO: extract this function, along with the 'theCompositionNoteCount' and 'theCompositionNotes'
+-- elements in the 'Composition' data type into their own data type in a separate module, because
+-- these fields and this function are a common mutable data structure used: a mutable buffer of
+-- items that grows exponentially and automatically as elements are added to it.
+incrementMutable
+  :: PrimMonad m
+  => Lens' (Composition m) (Mutable.MVector (PrimState m) elem)
+  -> Lens' (Composition m) Int
+  -> ComposeT m (Int, Mutable.MVector (PrimState m) elem)
+incrementMutable vector nelems = do
+  i       <- use nelems
+  nelems += 1
+  reqsize <- use nelems
+  vec     <- use vector
+  let allocsize = Mutable.length vec
+  vec     <- if reqsize < allocsize then return vec else do
+    vec <- lift $ Mutable.grow vec $ head $ dropWhile (<= reqsize) $ iterate (* 2) allocsize
+    vector .= vec
+    return vec
+  return (i, vec)
+
+-- | Register a pre-constructed note. 
+addNote :: PrimMonad m => Note -> ComposeT m NoteReference
+addNote note = do
+  (i, vec) <- incrementMutable compositionNotes compositionNoteCount
+  let noteID = NoteReference i
+  lift $ Mutable.write vec i $ note{ playedNoteID = noteID }
+  return noteID
+
 -- | Construct an arbitrary note, unconstrained by the key signature. But don't play the constructed
 -- note yet. Instead a reference to a note is returned which you can play, and if necessary, you can
 -- tie it to other notes. To construct a note that you never intend to tie and just play it
 -- immediately, use 'playNote' instead.
-notes :: Monad m => [KeyIndex] -> ComposeT m NoteReference
-notes _list = error "TODO: ProcGen.Music.Composition.notes"
+notes :: PrimMonad m => Strength -> [KeyIndex] -> ComposeT m NoteReference
+notes strength = addNote . makeNote untied strength
+  
+-- | Construct a rest.
+rest :: PrimMonad m => ComposeT m NoteReference
+rest = addNote RestNote
 
+-- | Construct a note tied to the given 'NoteReference'.
+tie :: PrimMonad m => Strength -> [KeyIndex] -> NoteReference -> ComposeT m NoteReference
+tie strength ns noteID@(NoteReference i) = do
+  tiedID <- addNote $ makeNote noteID strength ns
+  vec <- use compositionNotes
+  let untieLoop noteID@(NoteReference i) = unless (noteID == untied) $ do
+        prev <- Mutable.read vec i
+        when (playedTiedID prev == noteID) $ do
+          Mutable.write vec i $ prev{ playedTiedID = untied }
+          untieLoop $ playedTiedID prev
+  lift $ do
+    note <- Mutable.read vec i
+    untieLoop $ playedTiedID note
+    Mutable.write vec i $ note{ playedTiedID = tiedID }
+  return tiedID
 
+-- | Record a 'NoteReference' into the current 'Measure'. Only notes that are played, and not merely
+-- constructed by 'notes' or 'addNotes', will be converted to sound in the synthesizer.
+play :: Monad m => NoteReference -> ComposeT m ()
+play ref = compositionCurrentDiv %= (ref :) >> compositionDivSize  += 1
+
+-- | Construct a 'NoteReference' using 'notes' and immediately pass the reference to 'play' to have
+-- it recorded. 
+playNotes :: PrimMonad m => Strength -> [KeyIndex] -> ComposeT m NoteReference
+playNotes s n = notes s n >>= \ noteID -> play noteID >> return noteID
+
+-- | Construct a 'NoteReference' using 'tie' and immediately pass the referece to 'play' to have it
+-- recorded, then tie the note to a previouly 'play'ed 'NoteReference'.
+playTie :: PrimMonad m => Strength -> [KeyIndex] -> NoteReference -> ComposeT m NoteReference
+playTie s n k = tie s n k >>= \ noteID -> play noteID >> return noteID
+
+-- | Once the 'measure' function returns it constructs a 'Measure' but does not record it to the
+-- 'Composition'. This function records the 'Measure' into the 'Composition' so that the synthesizer
+-- can actually convert it to sound. Obviously you may 'score' the same 'Measure' as many times as
+-- you like, just be aware that people tend to find the same thing repeated again and again too many
+-- times to be boring.
+score :: PrimMonad m => Measure NoteReference -> ComposeT m ()
+score mesr = do
+  (i, vec) <- incrementMutable compositionMeasures compositionMeasureCount
+  lift $ Mutable.write vec i mesr
